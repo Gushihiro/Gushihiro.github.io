9fcd841e81cc9e61e264d51b224fd6fbb0a71a90
Hiroto Robinson <Hiroto.Robinson@claritymis.com>
2023-08-22 13:59:11 -0700

    Adjusted links, moved info for projects off of additional page and added styling

--- app/components/MainHero/mainHero.tsx
+++ app/components/MainHero/mainHero.tsx
@@ -12,17 +12,17 @@ export default function MainHero() {
   return (
     <div className="flex flex-row justify-center main-hero">
       <figure className="w-6/12 hero-img-box">
-        <div className="hero-circle"></div>
+        {/* <div className="hero-circle"></div> */}
         <img className="hero-img" src={"/portfolio-headerimg.png"} alt={"Hiroto"}/>
       </figure>
       <div className="inline-flex flex-col w-6/12 justify-between">
         <div className="flex flex-col">
-          <h1 className={`${sourceCodePro.className} text-5xl g-heading`}>Hiroto Robinson</h1>
+          <h1 className={`${sourceCodePro.className} text-6xl g-heading`}>Hiroto Robinson</h1>
           <div className="custom-divider small-divider self-end"></div>
-          <h2 className={`${sourceCodePro.className} text-3xl sub-heading self-end`}>Full-Stack Developer</h2>
+          <h2 className={`${sourceCodePro.className} text-3xl sub-heading self-end`}>Software Engineer</h2>
           <div className="w-10/12 self-end mt-9 text-right">
-            With 2 years of experience in Web Stacks involving Javascript, Typescript, 
-            React, NodeJS. Experienced in AGILE team environments collaborating to deliver custom ecommerce platforms to automate online business management and transactions.
+            Over 2 years of experience in Web Stacks involving Javascript, Typescript, 
+            React, & NodeJS. Experienced in AGILE team environments collaborating to deliver custom ecommerce platforms to automate online business management and transactions.
             Quick thinking, communicative, and always learning. I strive to improve myself each day and hold accountability for the code I push.
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,17 @@ export default function Page() {
         <div className="flex flex-col mt-9">
           <div className="flex justify-evenly">
             <div className="project-block" id="projects">
-              <Link href="/vedomy">
+              <Link href="https://gushihiro.github.io/Vedomy" target="_blank">
                 <img className="project-img" src="/vedomyView.png" alt="project"/>
-                <h4>Vedomy</h4>
+                <h4 className="project-heading">Vedomy</h4>
+                <h4 className="project-sub-heading">MySQL | Express | JavaScript | NodeJS</h4>
+                <h4 className="text-xl text-start mt-4">
+                  A Meditation App with Personal Wellness Tracking Applicability
+                  Track your mood, meditate using the animated breathing box, and receive new recipes, yoga poses, and motivational quotes daily.
+                </h4>
+                <h4 className="project-sub-text text-start mt-5">
+                  Additional Technologies: MomentJS jQuery Bootstrap Materialize
+                </h4>
               </Link>
             </div>
             {/* <div className="project-block">
@@ -37,9 +45,15 @@ export default function Page() {
               </Link>
             </div> */}
             <div className="project-block">
-              <Link href="/contactsCrud">
+              <Link href="https://gushihiro.github.io/contacts-crud/" target="_blank">
                 <img className="project-img" src="/contactCrudSS.png" alt="project"/>
-                <h4>Contacts-CRUD</h4>
+                <h4 className="project-heading">Contacts-CRUD</h4>
+                <h4 className="project-sub-heading">MySQL | Express | JavaScript | NodeJS</h4>
+                <h4 className="text-xl text-start mt-4">
+                  Simple, fully functioning CRUD Application Utilizing Axios-Imported API Contacts
+                  Track your mood, meditate using the animated breathing box, and receive new recipes, yoga poses, and motivational quotes daily.
+                </h4>
+                <h4 className="project-sub-text text-start mt-5">Additional Technologies: Material-Ui TypeScript Axios</h4>
               </Link>
             </div>
           </div>
