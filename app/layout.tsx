7178d25f358dd2c01639779ca904af1bae3c3b45
Hiroto Robinson <robinsonhiroto@gmail.com>
2024-04-25 23:16:07 -0700

    added logo and added meta tags for linkedin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { Navbar, Footer } from "./components/components";
+import Head from 'next/head';
 
 export default function RootLayout({
   children
@@ -9,6 +10,16 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+          <title>
+              Hiroto Robinson
+          </title>
+          <meta name={'title'} property={'og:title'} content={"Hiroto's Portfolio Site"} />
+          <meta name={'type'} property={'og:type'} content={'website'} />
+          <meta name={'image'} property={'og:image'} content={'/hiro_logo.svg'} />
+          <meta name={'url'} property={'og:url'} content={'https://gushihiro.vercel.app'} />
+          <meta name={'description'} property={'og:description'} content={'Hiroto is a full stack developer with a specialization in front-end development utilizing React, Nextjs, and various other front-end frameworks.'} />
+      </Head>
       <body className="h-full">
         <div className="flex flex-row">
           {children}
